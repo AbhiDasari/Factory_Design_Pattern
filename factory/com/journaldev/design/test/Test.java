b08365274062aa1f1034fdd4dffb08844e2613b7
AbhiDasari <srisaiabhishakegopal@gmail.com>
2019-02-01 10:31:29 +0530

    Initial Update

---
+++ factory/com/journaldev/design/test/Test.java
@@ -0,0 +1,13 @@
+package com.journaldev.design.test;
+import com.journaldev.design.factory.Factory;
+import com.journaldev.design.model.Computer;
+
+public class Test {
+	public static void main(String[] args) {
+		Computer pc = Factory.getComputer("pc","2 GB","500 GB","2.4 GHz");
+		Computer server = Factory.getComputer("server","16 GB","1 TB","2.9 GHz");
+		System.out.println("Factory PC Config::"+pc);
+		System.out.println("Factory Server Config::"+server);
+	}
+
+}
